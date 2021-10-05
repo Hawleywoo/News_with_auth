1cc916e8e37f71c11faf7f374da19f691164abce
Hawleywoo <alhawley21@gmail.com>
2021-10-04 23:50:04 -0600

    Rss feed and generate user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'home/index'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
