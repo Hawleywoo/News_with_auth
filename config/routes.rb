67cbad03b972c3ba422502f010f25e47429bc2cb
Hawleywoo <alhawley21@gmail.com>
2021-10-15 22:47:50 -0600

    User actions and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
   get 'users/new'
   get 'users/create'
   get 'home/index'
+  get 'home/login'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
